9b5e468ac0b67214318a6a92cb3e70eda6925a2f
Diego Said <diego@doublelogic.com.br>
2023-11-15 14:28:33 +0000

    Adds final message for the first challenge

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1><center>O exercício faz o mestre</center></h1>
+    return <h1><center>Toda jornada começa com o primeiro passo.</center></h1>
 }
 
 export default Home;
