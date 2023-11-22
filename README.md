33364d07327ea52acb07e77786eef768635fcaa3
Diego Said <diego@doublelogic.com.br>
2023-11-22 19:35:24 +0000

    Adds Prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # curso.dev
+
 Stuff that I am doing for the course https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1><center>Hello there!</center></h1>
+  return (
+    <h1>
+      <center>Hello there!</center>
+    </h1>
+  );
 }
 
 export default Home;
